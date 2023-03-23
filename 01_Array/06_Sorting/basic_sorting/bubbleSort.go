8cae9de75a4f31988ff7af9607e0747cb290298a
iamdevtry <tranbinhphongcmg@gmail.com>
2023-03-23 20:23:54 +0700

    update bubbleSort

--- 01_Array/06_Sorting/basic_sorting/bubbleSort.go
+++ 01_Array/06_Sorting/basic_sorting/bubbleSort.go
@@ -14,6 +14,24 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
+func bubbleSortWithPseudo(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		swapped := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			break
+		}
+	}
+
+	return arr
+}
+
 func main() {
 	arr := []int{1, 39, 2, 9, 7, 54, 11}
 
